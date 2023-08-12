608f0173745fd5407fd1354d60faaafe07d05726
Alberto <albertoulin619@gmail.com>
2023-08-11 18:37:05 -0600

    primer commit

--- migrations/1-facturas.js
+++ migrations/1-facturas.js
@@ -8,6 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      //probando
       pertenece_a: {
         type: Sequelize.STRING,
         allowNull: false
