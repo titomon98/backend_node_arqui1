8d068722bcc539aeee859428a9ff6626cd80c0a8
mwyss-orangeloops <mwyss@orangeloops.com>
2023-08-11 19:03:23 -0600

    Commit Inicial, definición de modelos

---
+++ models/sales_point/client.js
@@ -0,0 +1,11 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class clients extends Model {
+        static associate(models) {
+
+        }
+    };
+};
---
+++ models/sales_point/seller.js
@@ -0,0 +1,11 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class sellers extends Model {
+        static associate(models) {
+
+        }
+    };
+};
