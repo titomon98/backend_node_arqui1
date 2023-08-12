c1b6d0b1701c7cf016bb01ed49cbf72d3bd2733b
Alejandro Barrios <josebarriosrodas@gmail.com>
2023-08-11 18:51:36 -0600

    Primer commit

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 
-// facturas
-const facturasController = require('../controllers/cuenta/facturaControler');
-//personas
-const personasController = require('../controllers/personas/personaController')
-const puestoController = require('../controllers/personas/puestoController')
-//admin
-const adminController = require('../controllers/adminController')
-//RUTAS
+const testController = require('../controllers/testController');
+
 
 module.exports = (app) => {
     //facturas
-    router.get('/factura/find', facturasController.find);
-    router.get('/factura/find/id', facturasController.findById);
-    router.get('/factura/find/discount', facturasController.findByDiscount);
-    router.post('/factura/create', facturasController.create);
-    router.put('/factura/update', facturasController.update);
-    router.delete('/factura/delete/:id', facturasController.delete);
-    
-    //personas
-    router.get('/persona/find', personasController.find);
-
-    //puestos
-    router.get('/puesto/find', puestoController.findPuestos)
+    router.get('/hello', testController.sayHello)
     
     app.use('/', router);
 
