435978ef03ed15da426f6aa4ded838d6b5da63aa
danipro777 <daniel1603@umes.edu.gt>
2023-08-23 23:00:09 -0600

    arreglando

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,11 @@
 const { Router } = require('express');
 const router = Router();
 
-// facturas
-const facturasController = require('../controllers/cuenta/facturaControler');
-//personas
-const personasController = require('../controllers/personas/personaController')
-//admin
-const adminController = require('../controllers/adminController');
-const carroscontroller = require('../controllers/carros/carroscontroller');
+
 //RUTAS
 
 module.exports = (app) => {
-    //facturas
-    router.get('/factura/find', facturasController.find);
-    router.get('/factura/find/id', facturasController.findById);
-    router.get('/factura/find/discount', facturasController.findByDiscount);
-    
-    //personas
-    router.get('/persona/find', personasController.find);
-    router.get('/carros/find', carroscontroller.find);
-    router.get('/carros/find/id', carroscontroller.findById);
 
-    //create
-    router.post('/carros/create', carroscontroller.create);
-    //update
-    router.put('/carros/update', carroscontroller.update);
-    //delete
-    router.delete('/carros/delete/:id', carroscontroller.delete);
-
-    //inventario
-    //create
-
-    //cliente
     
     app.use('/', router);
 
