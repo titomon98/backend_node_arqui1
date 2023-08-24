013c4471aee5687986ea271dc0e86b57389f9228
danipro777 <daniel1603@umes.edu.gt>
2023-08-23 23:18:39 -0600

    migracion tablas con 2 FK

---
+++ migrations/7-detalleventas.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('detalle_ventas', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            id_venta: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'ventas',
+                    key: 'id'
+                }
+            },
+            cantidad: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            precio: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            id_inventario: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'inventarios',
+                    key: 'id'
+                }
+            },
+            createAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updateAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('detalle_ventas');
+    }
+};
